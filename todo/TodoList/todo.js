dd2d011f1c70cf3a58d5a2796d442626aab0a9ee
wusiqing <siqingiii@163.com>
2019-10-25 17:37:38 +0800

    休息一下 先提交

--- todo/TodoList/todo.js
+++ todo/TodoList/todo.js
@@ -1,1 +1,66 @@
-Vue.compon
+var todoContentList = [
+    {
+        content: '不同于组件和 prop，事件名不存在任何自动化的大小写转换。',
+        ifshow: true
+    },
+    {
+        content: '不同于组件和 prop，事件名不存在任何自动化的大小写转换。',
+        ifshow: true
+    },
+    {
+        content: '不同于组件和 prop，事件名不存在任何自动化的大小写转换。',
+        ifshow: true
+    },
+    {
+        content: '不同于组件和 prop，事件名不存在任何自动化的大小写转换。',
+        ifshow: true
+    },
+    {
+        content: '不同于组件和 prop，事件名不存在任何自动化的大小写转换。',
+        ifShow: true
+    }
+]
+
+Vue.component('delete-todo-button', {
+    props: ['ifshow', 'listindex'],
+    template: '<button @click="deleteTodo" v-show=ifshow>点击删除</button>',
+    methods: {
+        deleteTodo: function () {
+            console.log('listindex ---> s',this.listindex)
+            console.log('删除')
+        }
+    }
+})
+
+Vue.component('edit-todo-button', {
+    props: ['ifshow', 'listindex'],
+    template: '<button @click="editTodo" v-show=ifshow>点击修改当前项内容</button>',
+    methods: {
+        editTodo: function () {
+            console.log(':listindex=listindex', this.listindex)
+            console.log('编辑')
+        }
+    }
+})
+
+Vue.component('todo-list', {
+    props: ['todoList', 'listindex'],
+    template: "<div> <li>{{todoList.content}}</li><edit-todo-button :ifshow=todoList.ifshow :listindex=listindex></edit-todo-button><delete-todo-button :ifshow=todoList.ifshow :listindex=listindex></delete-todo-button></div>",
+    methods: {
+        deleteTodo: function () {
+            console.log('删除')
+            console.log('123', $index)
+        },
+        editTodo: function () {
+            console.log('编辑')
+        }
+    }
+})
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        todoContentList,
+        ifshow: true
+    }
+})
